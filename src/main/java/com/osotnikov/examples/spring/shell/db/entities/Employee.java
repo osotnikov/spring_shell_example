6e4e17a445fc4c211cb2e557e5af3d72a99d7051
Olegk Sotnikov <olegk_sotnikov@gmx.com>
2020-03-03 22:13:48 +0000

    mocking employee repo

--- src/main/java/com/osotnikov/examples/spring/shell/db/entities/Employee.java
+++ src/main/java/com/osotnikov/examples/spring/shell/db/entities/Employee.java
@@ -1,11 +1,15 @@
 package com.osotnikov.examples.spring.shell.db.entities;
 
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
 public class Employee {
 
-	private String educationLevel;
+	private EducationLevel educationLevel;
 	private int employeeId;
-	private String firstName;
-	private String lastName;
+	private String name;
 	private Department department;
+	private EmployementType employementType;
 }
--- src/main/java/com/osotnikov/examples/spring/shell/db/repository/EmployeeRepository.java
+++ src/main/java/com/osotnikov/examples/spring/shell/db/repository/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.osotnikov.examples.spring.shell.db.repository;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import com.osotnikov.examples.spring.shell.db.entities.EducationLevel;
 import org.springframework.stereotype.Repository;
 
 import com.osotnikov.examples.spring.shell.db.entities.Department;
@@ -14,8 +15,14 @@ public class EmployeeRepository {
 	Set<Employee> employees = new HashSet<>();
 
 	private void init() {
-		Employee employee = new Employee();
-		employee.setBirthDate(
+		Employee employee = Employee.builder()
+			.employeeId(0)
+			.department(Department.ACCOUNTING)
+			.educationLevel(EducationLevel.BACHELOR)
+			.firstName("James")
+			.lastName("Hetfield")
+			.build();
+	employee.setBirthDate(
 			new GregorianCalendar(2014, Calendar.FEBRUARY, 11).getTime());
 		employee.setDepartment(Department.ACCOUNTING);
 		employee.setEducationLevel("University");
@@ -27,7 +34,7 @@ public class EmployeeRepository {
 		return employees.stream().filter(employee -> {
 			if(payType != null) {
 				if() {
-					employee.getPosition();
+					employee.getr;
 				}
 			}
 			if(departmentId != null) {
