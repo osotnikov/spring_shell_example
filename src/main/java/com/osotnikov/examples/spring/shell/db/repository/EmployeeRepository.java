83164fa61a6e9619efcf0cf09cfd19e131c4ec03
Olegk Sotnikov <olegk_sotnikov@gmx.com>
2020-03-05 20:55:45 +0000

    mocking employee repo

--- src/main/java/com/osotnikov/examples/spring/shell/db/repository/EmployeeRepository.java
+++ src/main/java/com/osotnikov/examples/spring/shell/db/repository/EmployeeRepository.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import com.osotnikov.examples.spring.shell.db.entities.EducationLevel;
+import com.osotnikov.examples.spring.shell.db.entities.EmployementType;
 import org.springframework.stereotype.Repository;
 
 import com.osotnikov.examples.spring.shell.db.entities.Department;
@@ -15,18 +16,31 @@ public class EmployeeRepository {
 	Set<Employee> employees = new HashSet<>();
 
 	private void init() {
-		Employee employee = Employee.builder()
-			.employeeId(0)
-			.department(Department.ACCOUNTING)
-			.educationLevel(EducationLevel.BACHELOR)
-			.firstName("James")
-			.lastName("Hetfield")
-			.build();
-	employee.setBirthDate(
-			new GregorianCalendar(2014, Calendar.FEBRUARY, 11).getTime());
-		employee.setDepartment(Department.ACCOUNTING);
-		employee.setEducationLevel("University");
-		employees.add(employee);
+
+		employees.add(
+			Employee.builder()
+				.employeeId(0)
+				.department(Department.ACCOUNTING)
+				.educationLevel(EducationLevel.BACHELOR)
+				.employementType(EmployementType.CONTRACTOR)
+				.name("James Hetfield")
+				.build());
+		employees.add(
+			Employee.builder()
+				.employeeId(1)
+				.department(Department.ACCOUNTING)
+				.educationLevel(EducationLevel.COLLEGE)
+				.employementType(EmployementType.PERMANENT)
+				.name("Lars Ulrich")
+				.build());
+		employees.add(
+			Employee.builder()
+				.employeeId(2)
+				.department(Department.HUMAN_RESOURCES)
+				.educationLevel(EducationLevel.MASTERS)
+				.employementType(EmployementType.PERMANENT)
+				.name("Kirk Hamett")
+				.build());
 	}
 
 	public List<Employee> findEmployeesBy(String payType, Long departmentId, String educationLevel) {
